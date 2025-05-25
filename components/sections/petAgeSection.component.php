bb08b19c34ea5545eef44b3b9be3a26ba9109c5f
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-05-25 18:23:57 +0800

    feat: added a div to petAgeSection component to manipulate the section better

--- components/sections/petAgeSection.component.php
+++ components/sections/petAgeSection.component.php
@@ -2,22 +2,27 @@
 function petAgeSection($youngPets, $oldPets) {
     ?>
     <section id="age">
-        <h4>Youngest Pet</h4>
-        <?php
-        foreach($youngPets as $pet){
-            echo "<img src=\"{$pet["image"]}\">";
-            echo "<p>Name: {$pet["name"]}</p>";
-            echo "<p>Age: {$pet["age"]}</p>";
-        }
-        ?>
-        <h4>Oldest Pet</h4>
-        <?php
-        foreach($oldPets as $pet){
-            echo "<img src=\"{$pet["image"]}\">";
-            echo "<p>Name: {$pet["name"]}</p>";
-            echo "<p>Age: {$pet["age"]}</p>";
-        }
-        ?>
+        <div class="part">
+            <h4>Youngest Pet</h4>
+            <?php
+            foreach($youngPets as $pet){
+                echo "<img src=\"{$pet["image"]}\">";
+                echo "<p>Name: {$pet["name"]}</p>";
+                echo "<p>Age: {$pet["age"]}</p>";
+            }
+            ?>
+        </div>
+
+        <div class="part">
+            <h4>Oldest Pet</h4>
+            <?php
+            foreach($oldPets as $pet){
+                echo "<img src=\"{$pet["image"]}\">";
+                echo "<p>Name: {$pet["name"]}</p>";
+                echo "<p>Age: {$pet["age"]}</p>";
+            }
+            ?>
+        </div>
     </section>
     <?php
 }
