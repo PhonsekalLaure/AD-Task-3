5060b1a35086ddf340831a835ae7b7e0aa5c248d
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-05-25 17:15:26 +0800

    feat: add string and date manipulators to pets page

--- components/cards/displayPets.utils.php
+++ components/cards/displayPets.utils.php
@@ -16,9 +16,10 @@
             }
                 echo "<div class='pet' style='background-color: $backgroundcolor'>";
                 echo "<img src='{$pet['image']}' alt='{$pet['name']}'>";
-                echo "<h3>{$pet['name']}</h3>";
-                echo "<p>Color: {$pet['color']}</p>";
+                echo "<h3>" . strtoupper($pet['name']) . "</h3>";
+                echo "<p>Color: " . ucfirst($pet['color']) . "</p>";
                 echo "<p>Age: {$pet['age']} years old</p>";
+                echo "<p>Birthday: " . date("F d, Y", strtotime($pet["bday"])) . "</p>";
                 echo "</div>";
             }
     }
--- handlers/pageData.handler.php
+++ handlers/pageData.handler.php
@@ -6,22 +6,22 @@
     ];
 
     $cats = [
-        ["name" => "Oragne", "color" => "blue", "age" => 21, "bday" => "2004-03-15", "image" => "/pages/pets/assets/img/orange.gif"],
-        ["name" => "George Bush", "color"=> "white", "age" => 2, "bday" => "2023-03-15", "image" => "/pages/pets/assets/img/george.gif"],
-        ["name" => "Laughy", "color"=> "orange", "age" => 6, "bday" => "2019-03-15", "image"=> "/pages/pets/assets/img/laughy.gif"],
-        ["name"=> "Tung Tung", "color"=> "white", "age" => 8, "bday" => "2017-03-15", "image"=> "/pages/pets/assets/img/tungtung.gif"]
+        ["name" => "Oragne", "color" => "blue", "age" => 21, "bday" => "2004-03-19", "image" => "/pages/pets/assets/img/orange.gif"],
+        ["name" => "George Bush", "color"=> "white", "age" => 2, "bday" => "2023-08-5", "image" => "/pages/pets/assets/img/george.gif"],
+        ["name" => "Laughy", "color"=> "orange", "age" => 6, "bday" => "2019-09-1", "image"=> "/pages/pets/assets/img/laughy.gif"],
+        ["name"=> "Tung Tung", "color"=> "white", "age" => 8, "bday" => "2017-05-29", "image"=> "/pages/pets/assets/img/tungtung.gif"]
     ];
 
     $dogs = [
-        ["name" => "Browny", "color"=> "Brown", "age"=> 11, "bday" => "2014-03-15", "image" => "/pages/pets/assets/img/browny.gif"],
-        ["name"=> "Blud", "color"=> "Black", "age" => 19, "bday" => "2006-03-15", "image" => "/pages/pets/assets/img/blud.gif"],
-        ["name"=> "Snow", "color"=> "White", "age" => 5, "bday" => "2020-03-15", "image"=> "/pages/pets/assets/img/Snow.gif"],
-        ["name"=> "Cupcake", "color"=> "Brown", "age" => 7, "bday" => "2018-03-15", "image"=> "/pages/pets/assets/img/cupcake.gif"],
+        ["name" => "Browny", "color"=> "Brown", "age"=> 11, "bday" => "2014-01-30", "image" => "/pages/pets/assets/img/browny.gif"],
+        ["name"=> "Blud", "color"=> "Black", "age" => 19, "bday" => "2006-02-23", "image" => "/pages/pets/assets/img/blud.gif"],
+        ["name"=> "Snow", "color"=> "White", "age" => 5, "bday" => "2020-07-16", "image"=> "/pages/pets/assets/img/Snow.gif"],
+        ["name"=> "Cupcake", "color"=> "Brown", "age" => 7, "bday" => "2018-04-19", "image"=> "/pages/pets/assets/img/cupcake.gif"],
     ];
 
     $others = [
-        ["name"=> "Tralalero", "color"=> "Blue", "age" => 1000, "bday" => "1025-03-15", "image"=> "/pages/pets/assets/img/tralalero.gif"],
-        ["name"=> "Patapim", "color"=> "Green/Brown", "age" => 30, "bday" => "1995-03-15", "image"=> "/pages/pets/assets/img/patapim.gif"]
+        ["name"=> "Tralalero", "color"=> "Blue", "age" => 1000, "bday" => "1025-11-11", "image"=> "/pages/pets/assets/img/tralalero.gif"],
+        ["name"=> "Patapim", "color"=> "Green/Brown", "age" => 30, "bday" => "1995-10-10", "image"=> "/pages/pets/assets/img/patapim.gif"]
     ];
     $categories = [
         "cats" => $cats,
